8c2e5bb605a03232096a4e0eee0918ed8eaf370f
ethan-stone04 <ethan.m.stone-1@ou.edu>
2025-09-08 13:11:01 -0500

    Copied from my own program before github tutorial

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,7 +1,7 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Your Name: Ethan Stone
+// Did you seek help ? If yes, specify the helper or web link here: Slides for conversions
 
 using System;
 using System.Collections.Generic;
@@ -15,6 +15,41 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+
+             double Quantity;         
+             double Price;
+             double subtotal;
+             double salestax;
+             double salestotal;
+
+             const double tax = 8.5;
+
+             string Item;
+             string QuantityAsString;
+             string PriceAsString;
+
+             Console.WriteLine("What is the product name for what you are buying?");
+             Item = Console.ReadLine();
+             Console.WriteLine($"How many {Item}s are you buying?");
+             QuantityAsString = Console.ReadLine();
+             Console.WriteLine($"What is the price per {Item}");
+             PriceAsString = Console.ReadLine();
+
+             Quantity = Convert.ToDouble(QuantityAsString);
+             Price = Convert.ToDouble(PriceAsString);
+
+             subtotal = Price * Quantity;
+             salestax = tax * Quantity;
+             salestotal = subtotal + tax;
+
+             string subtotalAsString = subtotal.ToString("C2");
+             string salestaxAsString = salestax.ToString("C2");
+             string salestotalAsString = salestotal.ToString("C2");
+
+             Console.WriteLine($"Your subtotal is {subtotalAsString}");
+             Console.WriteLine($"Your sales tax is {salestaxAsString}");
+             Console.WriteLine($"Your total bill is {salestotalAsString}");
+            
         }
     }
 }
